d06f9a54ffaa8ffe389320c500ac4e5cd2f80cb2
josenggn <josenggn21@gmail.com>
2021-11-25 18:34:16 +0100

    CRUD creado para el endpoint de noticias

--- src/routes/news.js
+++ src/routes/news.js
@@ -2,37 +2,47 @@ var express = require('express');
 var router = express.Router();
 var News = require('../models/News.js')
 
-router.param('_id', (req, res, next, _id) => {
-    News.findOne({_id: _id}).then(function(news){
-        if (!news) { return res.sendStatus(404); }
-        req.news = news;
-        return next();
-    }).catch(next);
-})
+router.use(function timeLog(req, res, next) {
+    console.log('REQUEST /news', Date());
+    next();
+});
+
+router.get('/', (req, res) => {
+    News.find((err, news) => {
+        if (err) { return res.sendStatus(404); }
+        res.json(news);
+    });
+});
 
 router.get('/:id', (req, res) => {
     if (req.body) {
-
-        News.findById(req.body.id).then((news) => {
-
-            if (!news) { return res.sendStatus(404);}
-            return res.json({news: news})
-
+        News.findById(req.params.id).then((news) => {
+            if (!news) { return res.sendStatus(404); }
+            return res.json({ news: news })
         })
     }
 });
 
-router.get('/', (req, res) => {
-    res.send('hola')
-})
-
 router.post('/', (req, res) => {
-    
     News.create(req.body, function (err, news) {
         if (err) return next(err);
         res.json(news)
     });
-
 });
+
+router.put('/:id', (req, res) => {
+    News.findByIdAndUpdate(req.params.id, (err, news) => {
+        if (err) { return res.sendStatus(404); }
+        res.json(news);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    News.findByIdAndRemove(req.params.id, (err, news) => {
+        if (err) { return res.sendStatus(404); }
+        res.json(news);
+    });
+});
+
 
 module.exports = router;
