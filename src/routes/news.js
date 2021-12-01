60e3dac2d4455775639a321ec541ebfc989f3dec
josenggn <josenggn21@gmail.com>
2021-12-01 15:39:07 +0100

    Incluidos algunos codigos de estado. Arreglado put no actualizaba.

--- src/routes/news.js
+++ src/routes/news.js
@@ -10,15 +10,15 @@ router.use(function timeLog(req, res, next) {
 router.get('/', (req, res) => {
     News.find(req.query, (err, news) => {
         if (err) { return res.sendStatus(404); }
-        res.json(news);
+        res.status(200).json(news);
     });
 });
 
 router.get('/:id', (req, res) => {
     if (req.body) {
         News.findById(req.params.id).then((news) => {
-            if (!news) { return res.sendStatus(404); }
-            return res.json({ news: news })
+            if (news == null) { return res.sendStatus(404); }
+            res.status(200).json({ news: news })
         })
     }
 });
@@ -26,25 +26,21 @@ router.get('/:id', (req, res) => {
 router.post('/', (req, res) => {
     News.create(req.body, function (err, news) {
         if (err) return res.sendStatus(400);
-        res.json(news)
+        res.status(201).json(news)
     });
 });
 
 router.put('/:id', (req, res) => {
-    News.findByIdAndUpdate(req.params.id, (err, news) => {
-        if (err) { return res.sendStatus(404); }
-        res.json(news);
+    News.findByIdAndUpdate(req.params.id, req.body, (err, news) => {
+        if (err || news == null) { return res.sendStatus(404); }
+        res.status(200).json(news);
     });
 });
 
 router.delete('/:id', (req, res) => {
     News.findByIdAndRemove(req.params.id, (err, news) => {
-        if (err) { return res.sendStatus(404); }
-        if (news != null) { 
-            res.json(news); 
-        } else {
-            res.sendStatus(404);
-        }
+        if (err || news == null) { return res.sendStatus(404); }
+        res.status(200).json(news); 
     });
 });
 
