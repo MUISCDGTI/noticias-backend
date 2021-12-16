89c97c9d08b46b6192a84e2d47aed235505ddcce
josenggn <josenggn21@gmail.com>
2021-12-16 18:20:50 +0100

    Circuit breaker implemented

--- src/server.js
+++ src/server.js
@@ -5,13 +5,25 @@ var swaggerUi = require('swagger-ui-express'),
 var news = require('./routes/news')
 var health = require('./routes/health')
 
+var breaker = require('express-circuit-breaker')
+
+var CB = breaker({
+  catchError: e => 'trip',
+  handleBlockedRequest: (req, res) => res.sendStatus()
+})
+
 const API_BASE_PATH = '/api/v1'
 
 var app = express();
 
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use(API_BASE_PATH + '/news', news);
-app.use(API_BASE_PATH, health);
+app.use(API_BASE_PATH + '/news', CB,  news);
+app.use(API_BASE_PATH, CB,  health);
+
+app.get('/hola', CB, (req, res) => {
+    console.log('hola')
+    throw 1
+} )
 
 module.exports = app;
