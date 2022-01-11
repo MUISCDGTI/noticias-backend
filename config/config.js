9acd23e34bd0ca8b59e8be924323c9fe63fe5c5b
José Enrique Núñez <josenggn21@gmail.com>
2022-01-11 11:50:12 +0100

    Fix puerto

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 var config = {}
 
-config.PORT = process.env.PORT || 3000;
+config.PORT = 3000;
 config.HOST = process.env.HOST || '0.0.0.0';
 
 config.DB_URL = process.env.DB_URL || 'mongodb://localhost/news';
