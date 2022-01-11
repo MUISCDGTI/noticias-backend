50913487a904218e0943ac26f7a5b58e08eaf47b
José Enrique Núñez <josenggn21@gmail.com>
2022-01-11 12:20:27 +0100

    Configuración Mongo Atla

--- config/config.js
+++ config/config.js
@@ -3,6 +3,6 @@ var config = {}
 config.PORT = 3000;
 config.HOST = process.env.HOST || '0.0.0.0';
 
-config.MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost/news';
+config.MONGO_URL = process.env.MONGO_URL;
 
 module.exports = config;
