988f94e0ce43df1dfdbd1b0bdbc3a7984d26b1ce
José Enrique Núñez <josenggn21@gmail.com>
2022-01-15 16:26:42 +0100

    Pruebas corregir fallo test

--- src/models/News.js
+++ src/models/News.js
@@ -2,13 +2,13 @@ var mongoose = require('mongoose');
 
 var newsSchema = new mongoose.Schema({
   title: {type: String, required: true},
-  description: String,
+  //description: String,
   text: {type: String, required: true},
   author: {type: String, required: true},
-  createdAt: {type: Date, default: Date.now},
-  relatedMovies: {type: Array, default: []},
-  tags: {type: Array, default: []},
-  image: {type: String}
+  //createdAt: {type: Date, default: Date.now},
+  //relatedMovies: {type: Array, default: []},
+  //tags: {type: Array, default: []},
+  //image: {type: String}
 });
 
 module.exports = mongoose.model('News', newsSchema);
