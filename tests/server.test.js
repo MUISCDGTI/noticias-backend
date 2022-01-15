e474dc7c874f9ca685b99e7c2012294c141e39c1
José Enrique Núñez <josenggn21@gmail.com>
2022-01-15 15:49:21 +0100

    Test GET /news

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,3 +1,6 @@
+const News = require("../src/models/News");
+const app = require("../src/server");
+const request = require("supertest");
 
 describe("Hello world test", () => {
 
@@ -9,4 +12,63 @@ describe("Hello world test", () => {
         expect(sum).toBe(8);
     });
 
+});
+
+describe("News API", () => {
+
+    describe("GET /news", () => {
+
+        beforeAll(() => {
+
+            const news = [
+                {
+                    id: 1,
+                    title: "Nueva pelicula de Spiderman",
+                    text: "Proximamente en cines estará disponible la nueva película de Spiderman",
+                    createdAt: "2021-09-21T00:00:00.000Z",
+                    author: "Jose Enrique",
+                    tags: ["Accion", "Terror"],
+                    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png"
+                },
+                {
+                    id: 2,
+                    title: "Nueva pelicula de Batman",
+                    text: "Proximamente en cines estará disponible la nueva película de Batman",
+                    createdAt: "2021-09-21T00:00:00.000Z",
+                    author: "Jose Enrique",
+                    tags: ["Accion"],
+                    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png"
+                },
+                {
+                    id: 3,
+                    title: "Nueva pelicula de Nolan",
+                    text: "Proximamente en cines estará disponible la nueva película de Nolan",
+                    createdAt: "2021-09-21T00:00:00.000Z",
+                    author: "Jose Manuel",
+                    tags: ["Accion", "Terror"],
+                    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png"
+                },
+            ];
+
+            dbFind = jest.spyOn(News, "find");
+            dbFind.mockImplementation((query, sm, sort, callback) => {
+                callback(null, news);
+            });
+        });
+
+        it("Should return all news", () => {
+
+            return request(app)
+                .get("/api/v1/news")
+                .then((response) => {
+
+                    expect(response.statusCode).toBe(200);
+
+                    expect(dbFind).toBeCalledWith({}, expect.any(Function));
+
+                    expect(response => { console.log(response) })
+
+                });
+        });
+    });
 });
