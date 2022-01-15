c1efdcf7c45ec80d623259618555b9c866ae754e
José Enrique Núñez <josenggn21@gmail.com>
2022-01-15 16:27:23 +0100

    Pruebas corregir fallo test

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,41 +1,27 @@
 const News = require("../src/models/News.js");
 const app = require("../src/server.js");
 const request = require("supertest");
-
-describe("GET /", () => {
-    it("should return an HTML document", () => {
-      return request(app)
-        .get("/")
-        .then((response) => {
-          expect(response.status).toBe(200);
-          expect(response.type).toEqual(expect.stringContaining("html"));
-          expect(response.text).toEqual(expect.stringContaining("h1"));
+describe("News API", () => {
+    describe("GET /", () => {
+        it("should return an HTML document", () => {
+        return request(app)
+            .get("/")
+            .then((response) => {
+            expect(response.status).toBe(200);
+            expect(response.type).toEqual(expect.stringContaining("html"));
+            expect(response.text).toEqual(expect.stringContaining("h1"));
+            });
         });
     });
-  });
-
-describe("News API", () => {
 
     describe("GET /news", () => {
-
         beforeEach(() => {
-
             const news = [
                 {
                     title: "Nueva pelicula de Spiderman",
                     text: "Proximamente en cines estará disponible la nueva película de Spiderman",
                     author: "Jose Enrique",
-                },
-                {
-                    title: "Nueva pelicula de Batman",
-                    text: "Proximamente en cines estará disponible la nueva película de Batman",
-                    author: "Jose Enrique",
-                },
-                {
-                    title: "Nueva pelicula de Nolan",
-                    text: "Proximamente en cines estará disponible la nueva película de Nolan",
-                    author: "Jose Manuel",
-                },
+                }
             ];
 
             dbFind = jest.spyOn(News, "find");
