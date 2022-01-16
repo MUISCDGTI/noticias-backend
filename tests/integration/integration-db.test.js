bf0a0e5b87a9969c6320a1aa4e434372ebc015e4
José Enrique Núñez <josenggn21@gmail.com>
2022-01-16 20:08:09 +0100

    Pruebas integracion BD

---
+++ tests/integration/integration-db.test.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const dbConnect = require('../../src/db');
+const News = require('../../src/models/News')
+
+describe('News DB connection', () => {
+    beforeAll(() => {
+        return dbConnect();
+    })
+
+    beforeEach((done) => {
+        News.deleteMany({}, (err) => {
+            done();
+        });
+    });
+
+    it('Writes a news item in the DB', (done) => {
+        const news = new News({title: 'Vuelve spiderman', text: 'Prueba de noticia', author:'Jose'});
+        news.save((err, news) => {
+            expect(err).toBeNull();
+            News.find({}, (err, news) => {
+                expect(news).toBeArrayOfSize(1);
+                done();
+            });
+        });
+    });
+
+    afterAll((done) => {
+        mongoose.connection.db.dropDatabase(() => {
+            mongoose.connection.close(done);
+        });
+    });
+
+})
