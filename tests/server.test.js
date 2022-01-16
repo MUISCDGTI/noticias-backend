d1b69f999964100cb12514cf3ba4f76db4043e62
José Enrique Núñez <josenggn21@gmail.com>
2022-01-16 10:28:33 +0100

    Autorizacion en tests

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,6 +1,7 @@
 const News = require("../src/models/News.js");
 const app = require("../src/server.js");
 const request = require("supertest");
+const ApiKey = require("../src/models/apikeys.js");
 describe("News API", () => {
     describe("GET /", () => {
         it("should return an HTML document", () => {
@@ -25,16 +26,26 @@ describe("News API", () => {
 
             ];
 
+            const user = {
+                user: "test",
+                apikey: "1"
+            }
+
             dbFind = jest.spyOn(News, "find");
             dbFind.mockImplementation((query, callback) => {
                 callback(null, news);
             });
+
+            auth = jest.spyOn(ApiKey, "findOne");
+            auth.mockImplementation((query, callback) => {
+                callback(null, new ApiKey(user));
+            })
         });
 
         it("Should return all news", () => {
 
             return request(app)
-                .get("/api/v1/news")
+                .get("/api/v1/news").set('apikey', '1')
                 .then((response) => {
                     expect(response.statusCode).toBe(200);
                     expect(dbFind).toBeCalledWith({}, expect.any(Function));
