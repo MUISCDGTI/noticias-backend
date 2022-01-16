8f077eb286d89b39eff78774a375ef75fdc585e2
José Enrique Núñez <josenggn21@gmail.com>
2022-01-16 13:04:34 +0100

    Nuevo test para POST

--- tests/server.test.js
+++ tests/server.test.js
@@ -52,6 +52,70 @@ describe("News API", () => {
                 });
         });
     });
+
+    describe("GET /news BY ID", () => {
+        beforeAll(() => {
+          const news = 
+            {
+              _id: "61e3f651bb7477b9959fe4b1",
+              title: "Nueva pelicula de Spiderman",
+              text: "Proximamente en cines estará disponible la nueva película de Spiderman",
+              author: "Jose Enrique",
+            };
+    
+          const user = {
+            user : 'test',
+            apikey: '1'
+          };
+    
+          dbFindById = jest.spyOn(News, "findById");
+          dbFindById.mockImplementation((r, callback) => {
+            callback(null, news);
+          });
+    
+          auth = jest.spyOn(ApiKey, "findOne");
+          auth.mockImplementation((query, callback) => {
+            callback(null, new ApiKey(user));
+          });
+        });
+    
+        it("should return a news item by id", () => {
+          return request(app)
+            .get("/api/v1/news/61e3f651bb7477b9959fe4b1")
+            .set('apikey', '1')
+            .then((response) => {
+              expect(response.statusCode).toBe(200);
+              expect(response.body).toStrictEqual({
+                _id: "61e3f651bb7477b9959fe4b1",
+                title: "Nueva pelicula de Spiderman",
+                text: "Proximamente en cines estará disponible la nueva película de Spiderman",
+                author: "Jose Enrique"
+              });
+              expect(dbFindById).toBeCalledWith("61e3f651bb7477b9959fe4b1" ,
+                expect.any(Function)
+              );
+            });
+        });
+    
+        it("should not return any news due to incorrect id", () => {
+          dbFindById.mockImplementation((r, callback) => {
+            callback(`CastError: Cast to ObjectId failed for value "61e3f651bb7477b9959fe4b1" (type Object) at path "_id" for model "News"`, null);
+          });
+    
+          return request(app)
+            .get("/api/v1/news/61e3f651bb7477b9959fe4b2")
+            .set('apikey', '1')
+            .then((response) => {
+              expect(response.statusCode).toBe(500);
+              expect(response.body).toStrictEqual({});
+              expect(dbFindById).toBeCalledWith(
+               "61e3f651bb7477b9959fe4b1",
+                expect.any(Function)
+              );
+            });
+        });
+      });
+
     describe("POST /news", () => {
 
         const news = {
